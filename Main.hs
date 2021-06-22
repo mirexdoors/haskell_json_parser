67180338ee34a2e2b2f6e30c4cb0ebc3d6891846
mirexdoors <mirex.doors@mail.ru>
2021-06-23 02:39:24 +0700

    realize Functor Parser and Applicative Parser for usage string parser function

--- Main.hs
+++ Main.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ViewPatterns      #-}
+
 module Main where
+
+import Control.Applicative
+import Data.Char
 
 data JsonValue = JsonNull
                | JsonBool Bool
@@ -11,6 +17,20 @@ data JsonValue = JsonNull
 newtype Parser a = Parser
 	{ runParser ::  String -> Maybe (String, a) }
 
+instance Functor Parser where
+	fmap f (Parser p) =
+	 Parser $ \input -> do
+	  (input', x) <- p input
+	  Just (input', f x)
+
+instance Applicative Parser where
+	pure x = Parser $ \input -> Just (input, x)
+	(Parser p1) <*> (Parser p2) =
+	 Parser $ \input -> do
+		(input', f) <- p1 input
+		(input'', a) <- p2 input'
+		Just (input'', f a)
+
 jsonNull :: Parser JsonValue
 jsonNull = undefined
 
@@ -22,7 +42,8 @@ charP x = Parser $ f
 						 | otherwise = Nothing
 					  f [] = Nothing
 
-
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
 
 jsonValue :: Parser JsonValue
 jsonValue = undefined
